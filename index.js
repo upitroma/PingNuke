e94443b8c86608d382372c89e34bb4427a7e2d86
upitroma <55750919+upitroma@users.noreply.github.com>
2022-02-10 22:38:05 -0500

    wait for response between each ping

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ bot.login(secrets.TOKEN);
 console.log("starting bot...")
 
 const DISCORD_SERVER_ID = "732348936211136663"
-const PING_CHANNEL = "775031298371878942"
-
+const PING_CHANNEL = "775031298371878942"  
 
 bot.once("ready", async () => {
 
@@ -80,10 +79,12 @@ bot.once("ready", async () => {
                 }
             })
             lateUsers=args[0].value.split(" ")
+            startTime = new Date().getTime(); // keep track of time so the interval stops after 10 minutes
+            pingcannonActive=true
             pingSpam()
         }
         else if(command === 'stop'){
-            clearInterval(pingcannonInterval)
+            pingcannonActive=false
             bot.api.interactions(interaction.id, interaction.token).callback.post({
                 data: {
                     type: 4,
@@ -94,21 +95,36 @@ bot.once("ready", async () => {
             })
         }
         else if(command === 'remove'){
+            var callback = ""
             args[0].value.split(" ").forEach(element => {
                 if (lateUsers.includes(element)){
                     lateUsers.splice(lateUsers.indexOf(element),1)
+                    callback = callback= "stopped pinging "+args[0].value
+                }
+                else{
+                    callback= "could not find "+args[0].value+" in the list of users to ping"
                 }
             });
+            if(lateUsers.length==0){
+                pingcannonActive=false
+                callback= "no users left to ping. cease fire"
+            }
             bot.api.interactions(interaction.id, interaction.token).callback.post({
                 data: {
                     type: 4,
                     data: {
-                        content: "stopped pinging "+args[0].value
+                        content: callback
                     }
                 }
             })
-            if(lateUsers.length==0){
-                clearInterval(pingcannonInterval)
+            
+        }
+    });
+
+    bot.ws.on('MESSAGE_CREATE', async message => {
+        if (message.author.bot){
+            if (message.content.startsWith("./ping") && pingcannonActive){
+                pingSpam()
             }
         }
     });
@@ -116,18 +132,17 @@ bot.once("ready", async () => {
 
 var lateUsers=[]
 var pingcannonInterval //set to interval at runtime
+var pingcannonActive=false
+var startTime=0
 function pingSpam(){
-    var startTime = new Date().getTime(); // keep track of time so the interval stops after 10 minutes
 
-    pingcannonInterval=setInterval(() => {
-        bot.channels.cache.get(PING_CHANNEL).send(`./ping `+lateUsers.join(" "))
+    bot.channels.cache.get(PING_CHANNEL).send(`./ping `+lateUsers.join(" "))
 
-        //stop ping cannon after 10 minutes
-        if(new Date().getTime() - startTime > (10*60*1000)){ 
-            bot.channels.cache.get(PING_CHANNEL).send("stopping ping cannon...")
-            clearInterval(pingcannonInterval)
-        }
-    }, 1000);
+    //stop ping cannon after 10 minutes
+    if(new Date().getTime() - startTime > (10*60*1000)){ 
+        pingcannonActive=false
+        bot.channels.cache.get(PING_CHANNEL).send("10 minutes have passed. cease fire")
+    }
 }
 
 
